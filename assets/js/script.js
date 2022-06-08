537ca3ba946aed683e52933a0cdec90d440f9e42
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-08 14:37:02 -0400

    Removed commented out code in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-/* <div>
-<a href="https://github.com/Hreichgelt/Maps---The-Fingerprint-of-the-Past" target="_blank">
-    <img src="./assets/images/map-search.png" alt="Map Search" class="project">
-</a>
-
-<h3>Map Search Using APIs</h3>
-</div> */
-
 var projectsEl = document.querySelector('#project-section');
 
 var projectArray = [
