8a63b9e9ac47e4ed4cf056713c38309d11df2da7
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-08 14:24:01 -0400

    query selected project-section; created projectArray of project information; created and called init()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log('hello');
+/* <div>
+<a href="https://github.com/Hreichgelt/Maps---The-Fingerprint-of-the-Past" target="_blank">
+    <img src="./assets/images/map-search.png" alt="Map Search" class="project">
+</a>
+
+<h3>Map Search Using APIs</h3>
+</div> */
+
+var projectsEl = document.querySelector('#project-section');
+
+var projectArray = [
+    {
+        href: 'https://github.com/Hreichgelt/Maps---The-Fingerprint-of-the-Past',
+        src: './assets/images/map-search.png',
+        alt: 'Map Search',
+        title: 'Map Search Using APIs',
+    },
+    {
+        href: 'https://github.com/mycancel/password-generator',
+        src: './assets/images/password-generator.png',
+        alt: 'Password Generator',
+        title: 'Password Generator',
+    },
+]
+
+function init() {
+
+}
+
+init()
